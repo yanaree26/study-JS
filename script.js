c49b3230ecf9975dd61e6985793cf34e1c51b3fe
Yana Ree <yana261099@gmail.com>
2020-10-05 18:00:35 +0300

    Третья домашка

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let money = 500;
-let income = 75; 
-let addExpenses = 'Интернет, телефон, такси, развлечения'; 
-let deposit = false; 
+let money = prompt("Ваше месячный доход:", 500); 
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Интернет, телефон, такси'); 
+let deposit = confirm('Есть ли у вас депозит в банке?'); 
+let expenses1 = confirm('Введите обязательную статью расходов?');
+let expenses2 = confirm('Введите обязательную статью расходов?');
+let amount1 = prompt('Во сколько это обойдется?', 200);
+let amount2 = prompt('Во сколько это обойдется?', 200);
+
+let budgetMonth = money - amount1;
+
 let mission = 800; 
-let period = 3;
+let period = Math.round(mission / money);
 
 console.log(typeof money);
-console.log(typeof income);
 console.log(typeof deposit);
 
 console.log(addExpenses.length);
@@ -16,5 +21,15 @@ console.log(`Цель заработать ${mission} долларов`);
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = (money + income) / 30;
+let budgetDay = (budgetMonth) / 30;
 console.log(budgetDay);
+
+if(budgetDay >= 50) {
+    console.log('У вас высокий уровень дохода');
+} else if(budgetDay < 50 && budgetDay >= 10) {
+    console.log('У вас средний уровень дохода');
+} else if(budgetDay < 10) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if(budgetDay < 0) {
+    console.log('Что то пошло не так');
+}
