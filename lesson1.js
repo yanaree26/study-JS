950a4b6f95969da14c48050b2bdbdcf4dbb7fe65
Yana Ree <yana261099@gmail.com>
2020-10-05 16:34:31 +0300

    Второе дз

--- lesson1.js
+++ lesson1.js
@@ -1,10 +1,20 @@
-let money;
-let income; 
-let addExpenses; 
-let deposit; 
-let mission; 
-let period;
+let money = 500;
+let income = 75; 
+let addExpenses = 'Интернет, телефон, такси, развлечения'; 
+let deposit = false; 
+let mission = 800; 
+let period = 3;
 
-alert('lox');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('YANA');
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} долларов`);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = (money + income) / 30;
+console.log(budgetDay);
